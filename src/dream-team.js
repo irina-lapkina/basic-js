54367f8efb416c7669270e95418bd058f5fc47b3
irina-lapkina <infokotostudio@gmail.com>
2024-12-26 18:29:36 +0400

    feat: solve get-email and dream-team tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,36 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  const cleanedMembers = members
+    .filter((item) => typeof item === "string" && item.trim().length > 0)
+    .map((item) => item.trim());
+
+  if (cleanedMembers.length === 0) {
+    return false;
+  }
+
+  const letters = cleanedMembers.map((el) => el[0].toUpperCase()).sort();
+
+  return letters.join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
